ddd486eca60a37ffd9e026bc98a995b960eb2f10
James Yang <james.y.yang@cargosmart.com>
2019-04-29 11:20:47 +0800

    fixed notification criteria type

--- app/utils/ECDHHelper.js
+++ app/utils/ECDHHelper.js
@@ -158,7 +158,7 @@ class ECDHHelper {
      *
      * This signature will return "r","s" bigInt, order to support integration with Golang,
      * just can transform "r", "s" to Golang backend, Separate with a colon(":")
-     * @param {hex} hexData
+     * @param {string} hexData
      * @returns {string} signature base64 text, decode is "r:s" string; r,s -> hex
      */
     signatureByECDSAForGolang({hexData, privateKeyPointHex}) {
